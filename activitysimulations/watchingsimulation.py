070ef5cdcb939cc95e86d0c5d37e39cfdfd604fa
jane214 <jtan225@aucklanduni.ac.nz>
2020-09-03 17:18:25 +1200

    modified by Jane

--- activitysimulations/watchingsimulation.py
+++ activitysimulations/watchingsimulation.py
@@ -45,32 +45,32 @@ class MovieWatchingSimulation:
             self.__review_dict[user] = review
 
 
-user1 = User('Martin', 'pw12345')
-user2 = User('Ian', 'pw67890')
-movie = Movie("Moana", 2016)
-watchlist = WatchList()
-watchlist.add_movie(Movie("Moana", 2016))
-watchlist.add_movie(Movie("Ice Age", 2002))
-user1.watch_list = watchlist
-print("init user watch list", watchlist)
-watch_together = MovieWatchingSimulation(movie)
-print("movie to watch", watch_together.movie_to_watch)
-watch_together.add_user(user1)
-watch_together.add_user(user2)
-print(watch_together.size)
-print("after", user1.watch_list)
-review_text = "This movie was very enjoyable."
-rating = 8
-review = Review(movie, review_text, rating)
-review_text3 = "This movie was very enjoyable."
-rating3 = 8
-review3 = Review(movie, review_text3, rating3)
-watch_together = MovieWatchingSimulation(movie)
-list1 = []
-list1.append(user1)
-list1.append(user2)
-watch_together.get_together((list1))
-watch_together.write_review(user1, review_text, rating)
-watch_together.write_review(user2, review_text3, rating3)
-print(watch_together.review_dict[user1])
+# user1 = User('Martin', 'pw12345')
+# user2 = User('Ian', 'pw67890')
+# movie = Movie("Moana", 2016)
+# watchlist = WatchList()
+# watchlist.add_movie(Movie("Moana", 2016))
+# watchlist.add_movie(Movie("Ice Age", 2002))
+# user1.watch_list = watchlist
+# print("init user watch list", watchlist)
+# watch_together = MovieWatchingSimulation(movie)
+# print("movie to watch", watch_together.movie_to_watch)
+# watch_together.add_user(user1)
+# watch_together.add_user(user2)
+# print(watch_together.size)
+# print("after", user1.watch_list)
+# review_text = "This movie was very enjoyable."
+# rating = 8
+# review = Review(movie, review_text, rating)
+# review_text3 = "This movie was very enjoyable."
+# rating3 = 8
+# review3 = Review(movie, review_text3, rating3)
+# watch_together = MovieWatchingSimulation(movie)
+# list1 = []
+# list1.append(user1)
+# list1.append(user2)
+# watch_together.get_together((list1))
+# watch_together.write_review(user1, review_text, rating)
+# watch_together.write_review(user2, review_text3, rating3)
+# print(watch_together.review_dict[user1])
 
